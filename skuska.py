49ec75a957495877538bdc74092680d01748dc7a
pechom <peter.chomic996@gmail.com>
2019-10-14 22:02:07 +0200

    vysledky klasifikacii (max 98.96)

--- skuska.py
+++ skuska.py
@@ -7,85 +7,46 @@ import glob
 import csv
 import os
 import shutil
-
-# feature_path = 'C:/PycharmProjects/Diplomka/skusobny/classification/3-gram_bin_strings.csv'
-# standard_feature_path = 'C:/PycharmProjects/Diplomka/skusobny/classification/standard_3-gram_bin_strings.csv'
-# labels_path = 'C:/PycharmProjects/Diplomka/skusobny/classification/clear_labels2_head.csv'
-# output_dir = "C:/PycharmProjects/Diplomka/skusobny/selection/"
-#
-#
-# def gini(treshold):
-#     before = datetime.datetime.now()
-#     result = gini_index.gini_index(data, labels, mode="index")
-#     after = datetime.datetime.now()
-#     print("Gini")
-#     result = result[:treshold]
-#     print(len(result))
-#     print("cas: " + str(after - before))
-#     print('\n')
-#
-#
-# def fisher(treshold):
-#     before = datetime.datetime.now()
-#     result = fisher_score.fisher_score(data, labels, mode="index")
-#     after = datetime.datetime.now()
-#     print("fisher")
-#     result = result[:treshold]
-#     print(len(result))
-#     print("cas: " + str(after - before))
-#     print('\n')
-#
-#
-# def lap(treshold):
-#     before = datetime.datetime.now()
-#     result = lap_score.lap_score(data, mode="index")
-#     after = datetime.datetime.now()
-#     print("lap")
-#     result = result[:treshold]
-#     print(len(result))
-#     print("cas: " + str(after - before))
-#     print('\n')
-#
-#
-# def trace(treshold):
-#     before = datetime.datetime.now()
-#     result = trace_ratio.trace_ratio(data, labels, mode="index")
-#     after = datetime.datetime.now()
-#     print("trace")
-#     result = result[:treshold]
-#     print(len(result))
-#     print("cas: " + str(after - before))
-#     print('\n')
-#
-#
-# def numpy_load():
-#     labels = np.loadtxt(labels_path, delimiter=',', skiprows=1, dtype=np.int8)
-#     data = np.loadtxt(feature_path, delimiter=',', skiprows=1, dtype=np.int32)
-#     return data, labels
-#
-#
-# data, labels = numpy_load()
-# trace(treshold)
-# gini(treshold)
-# fisher(treshold)
-# lap(treshold)
-
-# pole = np.array([[0.1, 0.01], [0.005, 0.00004], [0.0007, 210.0], [25.0, 48.0], [0.001, 0.002]])
-# # print(pole)
-# for i in range(5):
-#     pole[i, :] = np.around(pole[i, :])
-#     print(pole[i, :])
+import lightgbm as lgb
 
 
-# print(pole)
-# print(np.around(pole))
-# print(np.array(pole*np.power(10, 3), dtype=np.int))
-# features='C:/PycharmProjects/Diplomka/skuska/features/*'
-# discrete='C:/PycharmProjects/Diplomka/skuska/discrete/'
-# original = 'C:/PycharmProjects/Diplomka/skuska/features/original/*'
-#
-# # shutil.rmtree(features[:-1])
-# os.renames(discrete[:-1], original[:-2])
+feature_path = 'features/simple.csv'
+labels_path = 'subory/clear_labels2_head.csv'
+rates = [0.05, 0.1, 0.2, 0.3]
+depths = [5, 6, 7, 8]
+leaves = [70, 80, 90]
+min_leaves = [5, 10, 15]
 
-# very_simple_file = 'C:/PycharmProjects/Diplomka/features/very_simple.csv'
-# print(set(np.loadtxt(very_simple_file, delimiter=',', max_rows=1, dtype="str")))
+
+def LGBM_goss(data, label, learning_rate, max_depth, num_leaves, min_leaf):
+    # https://lightgbm.readthedocs.io/en/latest/Parameters-Tuning.html
+    dtrain = lgb.Dataset(data=data, label=label)
+    param = {"max_depth": 7, "learning_rate": 0.2, "objective": 'multiclass', 'num_leaves': 80, 'boosting': 'goss',
+             "num_class": 10, "metric": 'multi_error', 'min_data_in_leaf': 10, 'num_threads': -1, 'verbosity': -1}
+    before = datetime.datetime.now()
+    result = lgb.cv(param, dtrain, num_boost_round=100, nfold=10, stratified=True, verbose_eval=None, shuffle=True)
+    after = datetime.datetime.now()
+    print("LGBM GOSS")
+    LGBM_print(result, before, after)
+
+
+def LGBM_print(result, before, after):
+    print("najlepsi priemer: " + str(1 - min(result['multi_error-mean'])))
+    print("index najlepsieho: " + str(result['multi_error-mean'].index(min(result['multi_error-mean']))))
+    print("najhorsi priemer: " + str(1 - max(result['multi_error-mean'])))
+    print("finalny priemer: " + str(1 - result['multi_error-mean'][-1]))
+    print("cas: " + str(after - before))
+    print('\n')
+
+
+labels = np.loadtxt(labels_path, delimiter=',', skiprows=1, dtype=np.uint8)
+data = np.loadtxt(feature_path, delimiter=',', skiprows=1, dtype=np.uint64)
+print("vsetky data: " + str(len(data[0])))
+print('\n')
+
+
+for rate in rates:
+    for depth in depths:
+        for leaf in leaves:
+            for min_leaf in min_leaves:
+                LGBM_goss(data, labels, rate, depth, leaf, min_leaf)
