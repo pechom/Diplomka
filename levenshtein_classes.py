80ca720ab1a9c46966940d488e8910a622cd6b2a
peter.chomic <peter.chomic@student.upjs.sk>
2019-10-30 23:00:07 +0100

    levenshtein detailed output, results

--- levenshtein_classes.py
+++ levenshtein_classes.py
@@ -1,7 +1,7 @@
 import Levenshtein
 
-f1 = open("subory/microsoft_names.txt", "r")
-f2 = open("subory/kaspersky_names.txt", "r")
+f1 = open("stare subory/microsoft_names.txt", "r")
+f2 = open("stare subory/kaspersky_names.txt", "r")
 
 list1 = f1.readlines()
 list2 = f2.readlines()
@@ -10,6 +10,6 @@ f2.close()
 
 for line1 in list1:
     for line2 in list2:
-        # if line1 == line2:
-        if Levenshtein.distance(line1, line2) < 3:
-            print(line1[:-1])
+        if 3 > Levenshtein.distance(line1, line2) > 1:
+            print(line1[:-1])  # posledny je znak konca riadku
+            print(line2[:-1])
